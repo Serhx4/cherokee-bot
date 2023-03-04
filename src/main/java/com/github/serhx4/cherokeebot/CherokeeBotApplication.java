f66dec1abd67f3abaf4de5e0b8dfafaa414056d6
Serhx4 <111274705+Serhx4@users.noreply.github.com>
2023-03-04 17:19:28 +0200

    Initial Commit

---
+++ src/main/java/com/github/serhx4/cherokeebot/CherokeeBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.serhx4.cherokeebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherokeeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherokeeBotApplication.class, args);
+    }
+
+}
